e026661be9bc3c62bcadef4c8db4e49768fda808
Stelus544 <74910358+Stelus544@users.noreply.github.com>
2025-10-23 18:27:31 -0500

    se arregla el modulo review.js

--- models/Review.js
+++ models/Review.js
@@ -1,10 +1,44 @@
-const mongoose = require('mongoose');
+const mongoose = require ('mongoose');
 
-const gameSchema = new mongoose.Schema({
-    gameId: { type: mongoose.Schema.Types.ObjectId, ref: 'Game', required: true },
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    reviewText: { type: String, required: true },
-    rating: { type: Number, min: 0, max: 5, required: true },
-}, { timestamps: true });
+const reviewSchema = new mongoose.Schema({
+    juegoId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Game',
+        required: true
+    },
+    titulo: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    contenido: {
+        type: String,
+        required: true,
+    },
+    puntuacion: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 5,
+    },
+    aspectosPositivos: {
+        type: [String],
+        default: []
+    },
+    aspectosNegativos: {
+        type: [String],
+        default: []
+    },
+    recomendado: {
+        type: Boolean,
+        required: true
+    },
+    fechaCreacion: {
+        type: Date,
+        default: Date.now
+    }
+}, {
+    timestamps: true
+});
 
-module.exports = mongoose.model('Review', gameSchema);
+module.exports = mongoose.model('Review', reviewSchema);
